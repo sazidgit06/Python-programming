e81149b28c511468006877a2861304c35497d575
sazid <sazidtec06@gmail.com>
2025-11-01 21:29:45 +0600

    class7 operators done

---
+++ class7/main.py
@@ -0,0 +1,9 @@
+# operators
+
+print(33333 + 44444) #addition
+print(33333 - 1111) #subtraction
+print(33333 / 34) #division
+print(33333 * 33333) #multiplication
+print(33333 % 43) #modulus
+print(5 ** 3) #Exponential
+print(33333 // 34) #Floor division
