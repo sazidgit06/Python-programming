5e42d7bdfc22f070a88b1ca0260c53fbec4fb132
sazid <sazidtec06@gmail.com>
2025-12-07 22:15:14 +0600

    class12 and class13 added

---
+++ class12/stringSlice.py
@@ -0,0 +1,5 @@
+name = "sazid, hasan"
+print(name[0:5])
+print(len(name))
+
+print(name[-5:-2])
---
+++ class13/stringMethods.py
@@ -0,0 +1,6 @@
+name = "!!!!!Sazid!!!!!!"
+print(name.rstrip("!"))
+print(name.lstrip("!"))
+print(name.upper())
+print(name.lower())
+print(name.replace("Sazid", "hasan"))
